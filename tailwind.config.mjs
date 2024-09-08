56431e01c009f66598982d37d405c7a6ad8e012a
Truong Minh Phat <tmphat1312@gmail.com>
2024-09-08 11:20:32 +0700

    feat: add dark mode toggle button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
+import twColors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["selector"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: twColors.gray[50],
+        fg: twColors.gray[900],
+        bg_dark: twColors.gray[900],
+        fg_dark: twColors.gray[50],
+      },
+    },
   },
   plugins: [],
 };
