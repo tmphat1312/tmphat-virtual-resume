a164c57a90c42c8c05596a6ebaa9232919e7ec4d
Truong Minh Phat <tmphat1312@gmail.com>
2024-09-09 11:51:59 +0700

    feat: add main section layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,18 @@
 import twColors from "tailwindcss/colors";
+import twTypography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["selector"],
   content: ["./src/**/*.{astro,html,js,jsx,md,ts,tsx}"],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        md: "64em",
+      },
+      padding: "0.5rem",
+    },
     extend: {
       colors: {
         bg: twColors.gray[50],
@@ -14,5 +22,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [twTypography],
 };
