b8ee3ea4a7e69a945f3e65d7b6daf2a2d6242bcd
Truong Minh Phat <tmphat1312@gmail.com>
2024-09-10 15:13:40 +0700

    chore: configure site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,20 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: getSiteUrl(),
   integrations: [tailwind(), icon()],
 });
+
+function getSiteUrl() {
+  if (process.env.ASTRO_SITE) return process.env.ASTRO_SITE;
+  if (process.env.NETLIFY) return process.env.URL;
+
+  if (process.env.CI && process.env.CI_CHECKS !== "true") {
+    console.error(
+      "Site URL not not found. Please set the ASTRO_SITE environment variable.",
+    );
+    process.exit(1);
+  }
+
+  return "http://localhost:4321";
+}
