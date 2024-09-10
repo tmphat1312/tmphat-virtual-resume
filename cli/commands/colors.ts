3327cc55ea6db996026768dc71fb016821c510b3
Truong Minh Phat <tmphat1312@gmail.com>
2024-09-10 19:33:59 +0700

    refactor: format and do something on colors command :))

--- cli/commands/colors.ts
+++ cli/commands/colors.ts
@@ -1,25 +1,45 @@
-import { exec } from "cli/helpers";
 import { writeFile } from "fs/promises";
 import { parseToHsl } from "polished";
 import colors from "tailwindcss/colors";
+
+import { exec, log } from "../helpers";
 
 type Shades = Record<
   50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950,
   string
 >;
 
-const dusks: Record<string, Shades> = {
-  ...pick(colors, ["slate", "gray", "zinc", "neutral", "stone"] as const),
-  // Add custom dusk presets here
-};
+const dusks: Record<string, Shades> = pick(colors, [
+  "slate",
+  "gray",
+  "zinc",
+  "neutral",
+  "stone",
+] as const);
 
-const accents: Record<string, Shades> = {
-  // prettier-ignore
-  ...pick(colors, ['rose', 'pink', 'fuchsia', 'purple', 'violet', 'indigo', 'blue', 'sky', 'cyan', 'teal', 'emerald', 'green', 'lime', 'yellow', 'amber', 'orange', 'red'] as const),
-  // Add custom accent presets here
-};
+const accents: Record<string, Shades> = pick(colors, [
+  "rose",
+  "pink",
+  "fuchsia",
+  "purple",
+  "violet",
+  "indigo",
+  "blue",
+  "sky",
+  "cyan",
+  "teal",
+  "emerald",
+  "green",
+  "lime",
+  "yellow",
+  "amber",
+  "orange",
+  "red",
+] as const);
 
 export async function colorsCommand() {
+  log.info("Generating color styles...");
+
   await Promise.all([
     ...Object.entries(dusks).map(([name, shades]) =>
       saveStyles(`dusk/${name}`, shadesToVars("dusk", shades)),
@@ -29,6 +49,8 @@ export async function colorsCommand() {
     ),
   ]);
   await exec("prettier --write src/styles/colors/**/*.astro --log-level error");
+
+  log.success("Color styles generated successfully!");
   process.exit(0);
 }
 
@@ -37,7 +59,12 @@ function shadesToVars(name: string, shades: Shades) {
     .toSorted(([num1], [num2]) => Number(num2) - Number(num1))
     .map(([num, value]) => {
       const { hue, saturation, lightness } = parseToHsl(value);
-      return `--${name}-${num}: ${Math.round(hue)} ${Math.round(saturation * 100)}% ${Math.round(lightness * 100)}%;`;
+      return (
+        `--${name}-${num}: ` +
+        `${Math.round(hue)} ` +
+        `${Math.round(saturation * 100)}% ` +
+        `${Math.round(lightness * 100)}%;`
+      );
     });
 }
 
@@ -52,7 +79,8 @@ function pick<T extends object, K extends keyof T>(
   obj: T,
   keys: K[],
 ): Pick<T, K> {
-  const result = {} as T;
-  keys.forEach((key) => (result[key] = obj[key]));
-  return result;
+  return keys.reduce((result, key) => {
+    result[key] = obj[key];
+    return result;
+  }, {} as T);
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,44 +1,98 @@
-import twColors from "tailwindcss/colors";
 import twTypography from "@tailwindcss/typography";
 import plugin from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["selector"],
   content: ["./src/**/*.{astro,html,js,jsx,md,ts,tsx}"],
   theme: {
-    container: {
-      center: true,
-      screens: {
-        md: "64em",
-      },
-      padding: "0.5rem",
-    },
     extend: {
-      colors: {
-        bg: twColors.gray[50],
-        fg: twColors.gray[900],
-        bg_dark: twColors.gray[900],
-        fg_dark: twColors.gray[50],
+      spacing: {
+        15: "3.75rem",
+        18: "4.5rem",
+        26: "6.5rem",
+        50: "12.5rem",
+      },
+      colors: getCustomColors(),
+      fontFamily: {
+        header: ["var(--font-header)", ...defaultTheme.fontFamily.sans],
+        body: ["var(--font-body)", ...defaultTheme.fontFamily.sans],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": "hsl(var(--text-primary))",
+            "--tw-prose-headings": "hsl(var(--text-title))",
+            "--tw-prose-lead": "hsl(var(--text-primary))",
+            "--tw-prose-links": "hsl(var(--text-title))",
+            "--tw-prose-bold": "hsl(var(--text-title))",
+            "--tw-prose-counters": "hsl(var(--text-secondary))",
+            "--tw-prose-bullets": "hsl(var(--text-secondary))",
+            "--tw-prose-hr": "hsl(var(--separator))",
+            "--tw-prose-quotes": "hsl(var(--text-title))",
+            "--tw-prose-quote-borders": "hsl(var(--border))",
+            "--tw-prose-captions": "hsl(var(--text-secondary))",
+            "--tw-prose-kbd": "hsl(var(--text-title))",
+            "--tw-prose-kbd-shadows": "hsl(var(--text-title))",
+            "--tw-prose-code": "hsl(var(--text-title))",
+            "--tw-prose-pre-code": "hsl(var(--bg-main))",
+            "--tw-prose-pre-bg": "hsl(var(--text-title))",
+            "--tw-prose-th-borders": "hsl(var(--separator))",
+            "--tw-prose-td-borders": "hsl(var(--border))",
+          },
+        },
       },
     },
   },
-  plugins: [
-    twTypography,
-    plugin(function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          "auto-fill": (value) => ({
-            gridTemplateColumns: `repeat(auto-fill, minmax(min(${value}, 100%), 1fr))`,
-          }),
-          "auto-fit": (value) => ({
-            gridTemplateColumns: `repeat(auto-fit, minmax(min(${value}, 100%), 1fr))`,
-          }),
-        },
-        {
-          values: theme("width", {}),
-        },
-      );
-    }),
-  ],
+  plugins: [twTypography, autoGridPlugin],
 };
+
+/* I don't know why it is not working, I will comeback later */
+function autoGridPlugin() {
+  return plugin(function ({ matchUtilities, theme }) {
+    matchUtilities(
+      {
+        "auto-fill": (value) => ({
+          gridTemplateColumns: `repeat(auto-fill, minmax(min(${value}, 100%), 1fr))`,
+        }),
+        "auto-fit": (value) => ({
+          gridTemplateColumns: `repeat(auto-fit, minmax(min(${value}, 100%), 1fr))`,
+        }),
+      },
+      {
+        values: theme("width", {}),
+      },
+    );
+  });
+}
+
+function getCustomColors() {
+  // Tailwind shades used for base colors.
+  const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
+
+  // Base color palettes used in the project. Related CSS variables are located in `src/styles/colors/(dusk|accent)`.
+  const baseColors = ["dusk", "accent"];
+
+  // Colors used in the code for styling. Related CSS variables are located in `src/styles/colors/elements`.
+  // prettier-ignore
+  const elementColors = ['button-bg', 'button-bg-contrast', 'web-heading-underline', 'pdf-heading-underline', 'primary-dark', 'primary', 'primary-light', 'primary-contrast', 'text-title', 'text-primary', 'text-secondary', 'text-contrast', 'icon-light', 'icon-main', 'icon-dark', 'border', 'separator', 'bg-body', 'bg-card', 'bg-popover', 'bg-tooltip', 'bg-light', 'bg-main', 'bg-dark'];
+
+  const result = {};
+
+  for (const color of baseColors) {
+    result[color] = Object.fromEntries(
+      shades.map((shade) => [
+        shade,
+        `hsl(var(--${color}-${shade}) / <alpha-value>)`,
+      ]),
+    );
+  }
+
+  result.color = {};
+  for (const color of elementColors) {
+    result.color[color] = `hsl(var(--${color}) / <alpha-value>)`;
+  }
+
+  return result;
+}
